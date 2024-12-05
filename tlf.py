0cd0cad638d2db8f02946489a038466a9edf7fe9
Wall3n <franmoreno1506@gmail.com>
2024-12-05 12:58:30 +0100

    Feat: Modulo de validación de telefono movil

---
+++ tlf.py
@@ -0,0 +1,24 @@
+import regex
+
+class TLF:
+    tlf1 = r"(\d{3})\s(\d{3})\s(\d{3})"
+    re_tlf = regex.compile(tlf1)
+
+    def __init__(self):
+        self.tlf = ''
+        self.validado = False
+
+    def validar_tlf(self, cadena):
+        if TLF.tlf_valido(cadena):
+            self.tlf = cadena
+            self.validado = True
+
+    @staticmethod
+    def tlf_valido(tlf):
+        match = re_tlf.fullmatch(tlf)
+        if match:
+            return True
+        else:
+            return False
+
+
