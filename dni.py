abf67f4a3b406fa4ba93c94bc572c838f119cd14
Wall3n <franmoreno1506@gmail.com>
2024-12-11 13:07:47 +0100

    Feat: Metodo de comparacion de DNIs estatico
    Fix: Inclusión de espacios en la expresion regular

--- dni.py
+++ dni.py
@@ -1,7 +1,7 @@
 import regex
 
-er_dni1 = r"(?P<formato1>(?P<num>\d{8})(?P<letra>[A-Z]))"
-er_dni2 = r"(?P<formato2>(?P<letra>[A-Z])-(?P<num>\d{8}))"
+er_dni1 = r"(?P<formato1>(?P<num>\d{8})(?P<letra>[A-Z]))\s*"
+er_dni2 = r"(?P<formato2>(?P<letra>[A-Z])-(?P<num>\d{8}))\s*"
 re_dni = regex.compile(f'{er_dni1}|{er_dni2}')
 
 class DNI:
@@ -13,8 +13,6 @@ class DNI:
     # Metodos de clase
     def validar_dni(self, cadena):
         if DNI.dni_valido(cadena):
-            self.numero = m['numero']
-            self.letra = m['letra']
             self.validado = True
 
     # Metodos de formato
@@ -26,17 +24,22 @@ class DNI:
     # Metodos estáticos
     @staticmethod
     def letra_dni(nif):
-        extrangeros = ['X', 'Y', 'Z']
+        extranjeros = ['X', 'Y', 'Z']
         dni = nif
-        if(nif[0] in extrangeros):
-            dni = str(extrangeros.index(nif[0])) + nif[1:]
+        if(nif[0] in extranjeros):
+            dni = str(extranjeros.index(nif[0])) + nif[1:]
         letras = ['T','R','W','A','G','M','Y','F','P','D','X','B', 'N', 'J','Z','S','Q','V','H','L','C','K','E']
         res = int(dni) % 23
         return letras[res]
 
-    # Rehacer
     @staticmethod
     def dni_valido(dni):
         match = re_dni.fullmatch(dni)
         return match and match['letra'] == DNI.letra_dni(match['num'])
+
+    @staticmethod
+    def is_equal(dni1, dni2):
+        m1 = re_dni.fullmatch(dni1)
+        m2 = re_dni.fullmatch(dni2)
+        return m1['num'] == m2['num'] and m1['letra'] == m2['letra']
 
