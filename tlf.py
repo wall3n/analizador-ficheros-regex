a1949669ce49f292b4e92a39c72b84e5ee508f00
Wall3n <franmoreno1506@gmail.com>
2024-12-11 18:33:20 +0100

    Feat: Inclusion del formato internacional en los numeros de telefono

--- tlf.py
+++ tlf.py
@@ -1,7 +1,9 @@
 import regex
 
-tlf1 = r"(\d{3})\s(\d{3})\s(\d{3})\s*"
-re_tlf = regex.compile(tlf1)
+tlf1 = r"(?P<formato1>(\d{3})\s(\d{3})\s(\d{3}))\s*"
+tlf2 = r"(?P<formato2>\+(\d\s?){9,14}\d)\s*"
+re_tlf = regex.compile(f'{tlf1}|{tlf2}')
+PREFIJO_DEFAULT = "+34 "
 
 class TLF:
 
@@ -11,7 +13,11 @@ class TLF:
 
     def validar_tlf(self, cadena):
         if TLF.tlf_valido(cadena):
-            self.tlf = cadena
+            m = re_tlf.fullmatch(cadena)
+            if m['formato1']:
+                self.tlf = PREFIJO_DEFAULT + m['formato1']
+            if m['formato2']:
+                self.tlf = m['formato2']
             self.validado = True
 
     @staticmethod
@@ -25,6 +31,8 @@ class TLF:
     def is_equal(tlf1, tlf2):
         m1 = re_tlf.fullmatch(tlf1)
         m2 = re_tlf.fullmatch(tlf2)
-        return m1[1] == m2[1] and m1[2] == m2[2] and m1[3] == m2[3]
+        tl1 = m1['formato2'] if m1['formato2'] else PREFIJO_DEFAULT + m1['formato1']
+        tl2 = m2['formato2'] if m2['formato2'] else PREFIJO_DEFAULT + m2['formato1']
+        return tl1 == tl2
 
 
