08cc62a47cb7d87415153d957f7ac6bf36aed4d2
Wall3n <franmoreno1506@gmail.com>
2024-12-13 09:11:40 +0100

    Docs: Documentacion adjunta

--- dni.py
+++ dni.py
@@ -5,23 +5,34 @@ er_dni2 = r"(?P<formato2>(?P<letra>[A-Z])-(?P<num>\d{8}))\s*"
 re_dni = regex.compile(f'{er_dni1}|{er_dni2}')
 
 class DNI:
+    """
+    Clase que representa y valida DNI's
+    """
+
     def __init__(self):
         self.numero = ''
         self.letra = ''
         self.validado = False
 
-    # Metodos de clase
+    """
+    Precondicion: cadena ha de ser un nif valido
+    Efecto: Valida la cadena y la almacena
+    Resultado: No retorna nada
+    """
     def validar_dni(self, cadena):
         if DNI.dni_valido(cadena):
             self.validado = True
 
-    # Metodos de formato
     def formato1(self):
         return f'{self.numero}{self.letra}'
     def formato2(self):
         return f'{self.letra}-{self.numero}'
 
-    # Metodos estáticos
+    """
+    Precondicion: nif ha de ser una cadena de texto que contenga un dni valido
+    Efecto: Calcula la letra correspondiente a un nif valido
+    Resultado: Retorna la letra correspondiente al nif
+    """
     @staticmethod
     def letra_dni(nif):
         extranjeros = ['X', 'Y', 'Z']
@@ -32,11 +43,21 @@ class DNI:
         res = int(dni) % 23
         return letras[res]
 
+    """
+    Precondicion: dni ha de ser una cadena de texto qeu contenga un nif valido
+    Efecto: Valida el nif
+    Resultado: Devuelve la validez del nif
+    """
     @staticmethod
     def dni_valido(dni):
         match = re_dni.fullmatch(dni)
         return match and match['letra'] == DNI.letra_dni(match['num'])
 
+    """
+    Precondicion: dn1 y dni2 han de ser cadenas de texto que contengan nif validos
+    Efecto: Comprueba las dos cadenas y comprueba su igualdad
+    Resultado: retorna la igualdad de los nif
+    """
     @staticmethod
     def is_equal(dni1, dni2):
         m1 = re_dni.fullmatch(dni1)
