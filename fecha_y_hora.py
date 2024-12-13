8dafb72130d74a9bb62998496df093cf5128a3c9
Wall3n <franmoreno1506@gmail.com>
2024-12-13 09:11:58 +0100

    fecha_y_hora.py

--- fecha_y_hora.py
+++ fecha_y_hora.py
@@ -6,7 +6,9 @@ f_formato3 = r"(?P<formato3>(?P<hora>\d{2}):(?P<minutos>\d{2}):(?P<segundos>\d{2
 er_tiempo = regex.compile(f'{f_formato1}|{f_formato2}|{f_formato3}')
 
 class FechaYHora:
-
+    """
+    Clase que representa instantes temporales, los valida y los compara
+    """
 
     def __init__(self):
         self.año = 0
@@ -17,6 +19,11 @@ class FechaYHora:
         self.segundos = 0
         self.validada = False
 
+    """
+    Precondición: cadena ha de ser una cadena que cumpla los formatos descritos
+    Efecto: Comprueba si la cadena es valida y la guarda en el objeto
+    Resultado: Devuelve un booleano si se ha completado la validación
+    """
     def validar_fecha(self, cadena):
         match = er_tiempo.fullmatch(cadena)
         if match:
@@ -56,6 +63,11 @@ class FechaYHora:
     def compare(self, fecha):
         return FechaYHora.compara_fechas(self.año, self.mes, self.dia, self.hora, self.minutos, self.segundos, fecha.año, fecha.mes, fecha.dia, fecha.hora, fecha.minutos, fecha.segundos)  
 
+    """
+    Precondición: cadena ha de ser una cadena válida
+    Efecto: Comprueba que la cadena es valida y comprueba segun el formato que las fechas y las horas sean válidas
+    Resultado: retorna un booleano con la validez de la fecha
+    """
     @staticmethod
     def fecha_valida(cadena):
         match = er_tiempo.fullmatch(cadena)
@@ -74,9 +86,20 @@ class FechaYHora:
                 return FechaYHora.fecha_correcta(match['ano'], match['mes'], match['dia']) and FechaYHora.hora_correcta(match['hora'], match['minutos'], match['segundos'])
         return False
 
+    """
+    Precondicion: año ha de ser un año valido
+    Efecto: Comprueba si el año es bisiesto mediante el cálculo matemático
+    Resultado: Devuelve verdadero en caso de que sea bisiesto
+    """
     @staticmethod
     def es_bisiesto(año):
         return año % 4 == 0 and (not(año % 100 == 0) or año % 400 == 0)
+
+    """
+    Precondicion: año mes dia han de ser enteros válidos
+    Efecto: Comprueba que todos los parametros sean realistas
+    Resultado: Si algun parametro no es correcto devuelve False en caso contrario True
+    """
     @staticmethod
     def fecha_correcta(año, mes, dia):
         año = int(año)
@@ -98,6 +121,11 @@ class FechaYHora:
             return False
         return True
 
+    """
+    Precondicion: hora minuto segundo han de ser enteros válidos
+    Efecto: Comprueba que todos los parametros sean realistas
+    Resultado: Si algun parametro no es correcto devuelve False en caso contrario True
+    """
     @staticmethod
     def hora_correcta(hora,minuto,segundo):
         hora = int(hora)
@@ -111,11 +139,21 @@ class FechaYHora:
             return False
         return True
 
+º   """
+    Precondicion: mes ha de ser un mes en ingles dentro del array
+    Efecto: Calcula el mes en formato entero
+    Resultado: Devuelve el valor entero de cada mes
+    """
     @staticmethod
     def transformar_mes_int(mes):
-        meses = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "november", "december"]
+        meses = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
         return meses.index(str(mes).lower()) + 1 
 
+    """
+    Precondicion: año,mes,dia,hora,min,seg han de ser instantes temporales expresados como enteros validos
+    Efecto: Itera las dos fechas para y comprueba la menor
+    Resultado: Si la fecha 1 es menor devuelve -1 en caso contrario 1
+    """
     @staticmethod
     def compara_fechas(año1,mes1,dia1,hora1,min1,seg1,año2,mes2,dia2,hora2,min2,seg2):
         if(año1 < año2):
